5b484d235b89f88eeb03d53462651b23bd581b14
Tanner Silva <t@tannersilva.com>
2022-05-02 13:19:05 -0500

    email can now be passed as an argument for certbot

--- Sources/wiremand/Execute/CertbotExecute.swift
+++ Sources/wiremand/Execute/CertbotExecute.swift
@@ -5,8 +5,8 @@ struct CertbotExecute {
     enum Error:Swift.Error {
         case unableToAcquireSSL([String], [String])
     }
-    static func acquireSSL(domain:String) async throws {
-        let acquireSSL = try await Command(bash:"sudo certbot certonly --webroot -w /var/www/html -n --agree-tos -m tsilva@escalantegolf.com -d \(domain)").runSync()
+	static func acquireSSL(domain:String, email:String = "tsilva@escalantegolf.com") async throws {
+        let acquireSSL = try await Command(bash:"sudo certbot certonly --webroot -w /var/www/html -n --agree-tos -m \(email) -d \(domain)").runSync()
         guard acquireSSL.succeeded == true else {
             throw Error.unableToAcquireSSL(acquireSSL.stdout.compactMap({ String(data:$0, encoding:.utf8) }), acquireSSL.stderr.compactMap({ String(data:$0, encoding:.utf8) }))
         }
