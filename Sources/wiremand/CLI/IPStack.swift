117de726a391539a8bff0546eb9d0c725e392bc3
Tanner Silva <t@tannersilva.com>
2023-01-27 16:51:03 -0600

    added CLI program: ipstack

---
+++ Sources/wiremand/CLI/IPStack.swift
@@ -0,0 +1,51 @@
+import ArgumentParser
+import QuickLMDB
+
+struct IPStack:ParsableCommand {
+	static let configuration = CommandConfiguration(
+		commandName:"ipstack",
+		subcommands:[SetAPIKey.self, GetAPIKey.self]
+	)
+
+	struct GetAPIKey:ParsableCommand {
+		static let configuration = CommandConfiguration(
+			abstract:"get the currently configured IPStack API key."
+		)
+		
+		@OptionGroup
+		var globals:CLI.GlobalCLIOptions
+		
+		mutating func run() throws {
+			let daemonDB = try DaemonDB(globals)
+			guard daemonDB.readOnly == false else {
+				throw CLI.Error.insufficientPrivilege
+			}
+			do {
+				let key = try daemonDB.ipdb.getIPStackKey()
+				print("\(key)")
+			} catch LMDBError.notFound {
+				print("IPStack not configured.")
+			}
+		}
+	}
+	
+	struct SetAPIKey:ParsableCommand {
+		static let configuration = CommandConfiguration(
+			abstract:"set the IPStack API key that wiremand will use."
+		)
+		
+		@OptionGroup
+		var globals:CLI.GlobalCLIOptions
+		
+		@Argument
+		var key:String
+		
+		mutating func run() throws {
+			let daemonDB = try DaemonDB(globals)
+			guard daemonDB.readOnly == false else {
+				throw CLI.Error.insufficientPrivilege
+			}
+			try daemonDB.ipdb.setIPStackKey(key)
+		}
+	}
+}
