562ae5dfd945065688573b2d521fc076c56bdfc2
Tanner Silva <t@tannersilva.com>
2023-01-28 00:33:38 -0600

    tweaks to time values

--- Sources/wiremand/Extensions/Date.swift
+++ Sources/wiremand/Extensions/Date.swift
@@ -5,9 +5,9 @@ extension Date {
 		var time = self.timeIntervalSince(nowTime)
 		if (time > 0) {
 			switch time {
-			case 0..<3599:
+			case 0..<3600:
 				return ("In \(Int(time/60)) Minutes")
-			case 3599..<86399:
+			case 3600..<86399:
 				return time <= 7200 ? ("In 1 Hour"): ("In \(Int(time/3600)) Hours")
 			default:
 				return time <= 172800 ? ("In 1 Day"): ("In \(Int(time/86400)) Days")
@@ -15,9 +15,9 @@ extension Date {
 		} else {
 			time = abs(time)
 			switch time {
-			case 0..<3599:
+			case 0..<3600:
 				return ("\(Int(time/60)) Minutes Ago")
-			case 3599..<86399:
+			case 3600..<86399:
 				return time <= 7200 ? ("1 Hour Ago"): ("\(Int(time/3600)) Hours Ago")
 			default:
 				return time <= 172800 ? ("1 Day Ago"): ("\(Int(time/86400)) Days Ago")
