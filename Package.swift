aa8c017dc041b935d33902b91117e9dffb94c5b4
tannerdsilva <t@tannersilva.com>
2025-07-09 14:33:12 -0500

    revised bedrock dependency to solve build issues on linux

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         .package(url:"https://github.com/apple/swift-argument-parser.git", "1.5.1"..<"2.0.0"),
 		.package(url:"https://github.com/tannerdsilva/SwiftSlash.git", "4.0.0"..<"5.0.0"),
 		.package(url:"https://github.com/tannerdsilva/QuickLMDB.git", "11.1.0"..<"12.0.0"),
-		.package(url:"https://github.com/tannerdsilva/bedrock.git", "4.0.2"..<"5.0.0"),
+		.package(url:"https://github.com/tannerdsilva/bedrock.git", "4.0.3"..<"5.0.0"),
 		.package(url:"https://github.com/hummingbird-project/hummingbird.git", "2.14.1"..<"3.0.0"),
 		.package(url:"https://github.com/tannerdsilva/rawdog.git", "17.0.1"..<"18.0.0"),
 		.package(url:"https://github.com/swift-server/async-http-client.git", "1.26.1"..<"2.0.0"),
